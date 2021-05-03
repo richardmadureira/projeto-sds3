0415e1b0b66b8601661f91e556e249745cbe6b90
Richard Mendes Madureira <richard.madureira@dataprev.gov.br>
2021-05-03 09:26:47 -0300

    Primeiro dia

--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -4,7 +4,7 @@ const Footer = () => {
     return (
         <footer className="footer mt-auto py-3 bg-dark">
             <div className="container">
-                <p className="text-light">App desenvolvido por <a href="https://github.com/acenelio" target="_blank" rel="noreferrer">Richard Mendes Madureira</a></p>
+                <p className="text-light">App desenvolvido por <a href="https://github.com/richardmadureira" target="_blank" rel="noreferrer">Richard Mendes Madureira</a></p>
                 <p className="text-light"><small><strong>Semana Spring React</strong><br />
             Evento promovido pela escola DevSuperior: <a href="https://twitter.com/ricmadureira" target="_blank" rel="noreferrer">@ricmadureira</a></small></p>
             </div>
